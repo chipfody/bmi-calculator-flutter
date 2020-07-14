70612dd8aefff675f9d2a526313da805b178f02a
chipfody <60615532+chipfody@users.noreply.github.com>
2020-07-14 13:15:42 -0400

    adding custom design

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,15 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0XFF0A0E21),
+        scaffoldBackgroundColor: Color(0XFF0A0E21),
+        accentColor: Colors.purple,
+        textTheme: TextTheme(
+          // ignore: deprecated_member_use
+          body1: TextStyle(color: Colors.white), // Can also use Color(0xFFFFFFFF) for white text
+        ),
+      ),
       home: InputPage(),
     );
   }
